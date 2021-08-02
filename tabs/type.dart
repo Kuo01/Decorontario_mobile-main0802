f5723b12b71e1c323fcf3450aab3dd7912b6be28
Kuo01 <86921341+Kuo01@users.noreply.github.com>
2021-08-02 12:51:40 +0800

    Add files via upload

---
+++ tabs/type.dart
@@ -0,0 +1,28 @@
+var search_image=[
+  'assets/images/chair1.png',
+  'assets/images/chair2.png',
+  'assets/images/chair3.png',
+  'assets/images/chair4.png',
+  'assets/images/chair5.png',
+  'assets/images/chair6.png',
+  'assets/images/chair7.png',
+  'assets/images/chair8.png'
+
+];
+
+const  serchList=[
+  "cactus",
+  "chair",
+  "table football",
+  "table",
+  "sheen chair",
+  "plant",
+  "mixer",
+  "frame"
+];
+
+const  reecntSuggest=[
+  "suggest-1",
+  "suggest-2"
+];
+
